350de390c0a6c07e4b415e26d673265f7b9abce7
Shannon Patterson <23106944+shannon-patterson@users.noreply.github.com>
2023-12-07 12:33:41 -0500

    remove the tests. They're not really good tests, should write better tests

--- tests/addToVLCTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('../EnqueueToVLCTrigger.php');
-
-
-$file = "F:\sorting\music\Pearl Jam - 2005-09-13 Hamilton - 05 - Sad.mp3";
-
-$trigger = new \CurationPipeline\EnqueueToVLCTrigger($file);
-$trigger->run();
--- tests/artistCompleteTriggerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('../ArtistCompleteTrigger.php');
-
-$artistPath = "F:\\sorting\\music\\41st And Home";
-$sourcePath = "F:\\sorting\\source";
-
-$trigger = new \CurationPipeline\ArtistCompleteTrigger($artistPath, $sourcePath);
-$trigger->run();
